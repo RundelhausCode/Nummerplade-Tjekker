46d9bde7ab1497e4bb3ace97c062d23eb300319d
Jonas <jona345e@edu.sde.dk>
2020-01-07 15:25:42 +0100

    7/1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,33 @@ namespace Nummerplade_Tjekker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string URL = @"https://v1.motorapi.dk/vehicles/";
+        private string para = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
+            var search = new SearchWindow();
+            bool? dialogResult = search.ShowDialog();
+            switch (dialogResult)
+            {
+                case true:
+
+                    break;
+                case false:
+                case null:
+                default:
+                    Application.Current.Shutdown();
+                    break;
+            }
+
+
+        }
+        private void APICall()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders
+            
         }
     }
 }
---
+++ SearchWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nummerplade_Tjekker
+{
+    /// <summary>
+    /// Interaction logic for SearchWindow.xaml
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        public string reg = string.Empty;
+        public string vin = string.Empty;
+        public SearchWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Regbutton_Click(object sender, RoutedEventArgs e)
+        {
+            RegControl();
+            DialogResult = true;
+        }
+
+        private void Vinbutton_Click(object sender, RoutedEventArgs e)
+        {
+            VinControl();
+            DialogResult = true;
+        }
+        private bool RegControl()
+        {
+            reg = Regbox.Text;
+            return true;
+        }
+        private bool VinControl()
+        {
+            vin = Vinbox.Text;
+            return true;
+        }
+
+
+    }
+}
